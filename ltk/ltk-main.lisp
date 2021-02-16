9eb6bf972c266a9c57085dd6d3130be0a9e2560c
rn7s2 <a3788670@foxmail.com>
2021-02-16 14:08:14 +0800

    update

--- ltk/ltk-main.lisp
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sbcl --script
-
-(load "ltk/ltk")
-(in-package :ltk)
-(ltktest)
---
+++ prime-ltk.lisp
@@ -0,0 +1,38 @@
+#!/usr/bin/sbcl --script
+
+(load "ltk/ltk")
+(in-package :ltk)
+
+(with-ltk()
+  (defun get-prime-num (n)
+    (let* ((prime (make-array (+ n 1) :fill-pointer 0))
+	   (is-prime (make-array (+ n 1) :initial-element t)))
+      (setf (aref is-prime 0) nil)
+      (setf (aref is-prime 1) nil)
+      (do ((i 2 (+ i 1)))
+	  ((> i n) t)
+	(if (aref is-prime i)
+	    (vector-push i prime))
+	(block break
+	  (dotimes (j (length prime))
+	    (if (> (* i (aref prime j)) n)
+		(return-from break t))
+	    (setf (aref is-prime (* i (aref prime j))) nil)
+	    (if (= (mod i (aref prime j)) 0)
+		(return-from break t)))))
+      (length prime)))
+  
+  (let* ((frame (make-instance 'frame :master nil))
+	 (label (make-instance 'label :master frame :text "输入上界："))
+	 (spin (make-instance 'spinbox :master frame :increment 1 :to 10000 :from 2))
+	 (button (make-instance 'button :master frame :text "确定"
+				:command (lambda ()
+					   (do-msg
+					       (format nil "小于等于~A的质数共有~A个"
+						       (text spin) (get-prime-num (parse-integer (text spin))))
+					     :title "结果"
+					     :parent frame)))))
+    (pack frame)
+    (pack label :side :top)
+    (pack spin)
+    (pack button)))
