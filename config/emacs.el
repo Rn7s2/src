dd9f8034b0e73341e601ad8cc374637db94146fa
rn7s2 <a3788670@foxmail.com>
2021-03-14 19:06:16 +0800

    update

--- config/emacs.el
+++ config/emacs.el
@@ -8,23 +8,35 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-enabled-themes '(adwaita))
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes '(misterioso))
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages '(markdown-preview-eww markdown-mode))
+ '(package-selected-packages '(cnfonts d-mode))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 161 :width normal)))))
+
+;; Chinese fonts (for Windows only)
+(require 'cnfonts)
+(cnfonts-enable)
 
 ;; Tab 4
 (setq-default indent-tabs-mode nil)
 (setq c-basic-offset 4)
 (setq c-default-style "linux")
 (setq default-tab-width 4)
+
+;; Common Lisp
+(defun load-lisp ()
+  (interactive)
+  (load (expand-file-name "c:/Users/rn7s2/quicklisp/slime-helper.el"))
+  (setq inferior-lisp-program "sbcl")
+  (slime))
 
 ;; Full screen
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
