8406ad48291de56fdc0633bfc4e2367e38cf7cf2
rn7s2 <a3788670@foxmail.com>
2021-03-28 20:44:32 +0800

    update

---
+++ racket/prime.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(let* ((n (read))
+       (cnt 0)
+       (prime (make-vector n 0))
+       (is-prime (make-vector n #t)))
+
+  (define (get-prime)
+    (set! cnt 0)
+    (vector-set! is-prime 0 #f)
+    (vector-set! is-prime 1 #f)
+
+    (for ([i (in-range 2 n)])
+      (when (vector-ref is-prime i)
+        (set! cnt (add1 cnt))
+        (vector-set! prime cnt i))
+
+      (for ([j (in-range 1 (add1 cnt))])
+        #:break (>= (* i (vector-ref prime j)) n)
+        (vector-set! is-prime (* i (vector-ref prime j)) #f)
+        #:break (= (modulo i (vector-ref prime j)) 0)
+        #t)))
+
+  (define (print-prime)
+    (display (format "Total prime numbers: ~A~%" cnt))
+    (for ([i (in-range 1 (add1 cnt))])
+      (display (format "~A " (vector-ref prime i)))))
+
+  (time (get-prime))
+  ;;(print-prime)
+  (display (format "Total prime numbers: ~A~%" cnt)))
