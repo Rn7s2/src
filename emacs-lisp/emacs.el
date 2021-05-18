4d3eb5366cec0f85381bdaf25676777fda749bfd
rn7s2 <a3788670@foxmail.com>
2021-05-18 20:15:47 +0800

    update

--- emacs-lisp/emacs.el
+++ emacs-lisp/emacs.el
@@ -1,6 +1,6 @@
-(setq package-archives '(("gnu"   . "http://cmcc.mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://cmcc.mirrors.ustc.edu.cn/elpa/melpa/")
-			 ("melpa-stable" . "http://cmcc.mirrors.ustc.edu.cn/elpa/melpa-stable/")))
+(setq package-archives '(("gnu"   . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
+			 ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -12,18 +12,14 @@
  '(custom-enabled-themes '(misterioso))
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages '(cnfonts d-mode))
+ '(package-selected-packages '(company racket-mode markdown-mode))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 161 :width normal)))))
-
-;; Chinese fonts (for Windows only)
-(require 'cnfonts)
-(cnfonts-enable)
+ )
 
 ;; Tab 4
 (setq-default indent-tabs-mode nil)
@@ -31,15 +27,12 @@
 (setq c-default-style "linux")
 (setq default-tab-width 4)
 
-;; Common Lisp
-(defun load-lisp ()
-  (interactive)
-  (load (expand-file-name "c:/Users/rn7s2/quicklisp/slime-helper.el"))
-  (setq inferior-lisp-program "sbcl")
-  (slime))
-
 ;; Full screen
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 ;; Backups goto ~/.backups
 (setq backup-directory-alist (quote (("." . "~/.emacs_backups"))))
+
+;; company
+(add-hook 'after-init-hook'global-company-mode)
+(setq company-clang-arguments '())
